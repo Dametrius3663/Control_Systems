b660c03d8d0fcb38c3d970959100f61862b65ae0
Dametrius3663 <dametrius3663@gmail.com>
2026-04-27 15:18:03 -0400

    s

--- Rover2.py
+++ Rover2.py
@@ -83,7 +83,7 @@ def track_marker_pnp(rvec, tvec, reverse=False):
 
     yaw = np.arctan2(x, z)
 
-    steer = np.degrees(yaw) * 1.002
+    steer = np.degrees(yaw) * 0.002
 
     if reverse:
         steer *= -1
