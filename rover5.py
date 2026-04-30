0dd912b99bb81ae22e7f07191ff2afcc2058dcc2
Dametrius3663 <dametrius3663@gmail.com>
2026-04-30 13:58:12 -0400

    tuning

--- rover5.py
+++ rover5.py
@@ -114,6 +114,13 @@ def AtMarker10():
     print("Marker 10 → TURN")
     px.set_dir_servo_angle(-45)
     px.forward(update_speed(speed))
+    time.sleep(2)
+    px.set_dir_servo_angle(-4)
+    px.forward(update_speed(speed))
+    time.sleep(5.5)
+    px.set_dir_servo_angle(-10)
+    px.forward(update_speed(speed))
+    time.sleep(0.9)
 
 def AtMarker11():
     print("Marker 11 → STRAIGHT")
@@ -196,13 +203,6 @@ def track_marker_pnp(rvec, tvec, reverse=False):
 
         elif target == 10:
             AtMarker10()
-            time.sleep(2)
-            px.set_dir_servo_angle(-4)
-            px.forward(update_speed(speed))
-            time.sleep(6.5)
-            px.set_dir_servo_angle(-10)
-            px.forward(update_speed(speed))
-            time.sleep(0.5)
             stop_car()
 
         elif target == 11:
