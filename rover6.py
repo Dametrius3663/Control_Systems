532e78099b304b1489528e0e897b25b3d067f299
Dametrius3663 <dametrius3663@gmail.com>
2026-05-05 08:57:48 -0400

    tuning

--- rover6.py
+++ rover6.py
@@ -102,15 +102,15 @@ def AtMarker10():
     px.set_dir_servo_angle(-45)
     px.set_motor_speed(1,(speed)*0.2)
     px.set_motor_speed(2, (-speed))
-    time.sleep(2.2)
+    time.sleep(1.2)
     px.set_dir_servo_angle(-4)
     px.set_motor_speed(1,(speed))
     px.set_motor_speed(2, (-speed))
-    time.sleep(6.5)
+    time.sleep(4.5)
     px.set_dir_servo_angle(-20)
     px.set_motor_speed(1,(speed)*0.175)
     px.set_motor_speed(2, (-speed))
-    time.sleep(2.2)
+    time.sleep(0.6)
 
 def AtMarker11():
     print("Marker 11 → STRAIGHT")
