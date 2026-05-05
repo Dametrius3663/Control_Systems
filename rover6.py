2dcf06909ac42a377983e2d72dcf02fd921ce2b4
Dametrius3663 <dametrius3663@gmail.com>
2026-05-05 09:42:09 -0400

    tuned 4

--- rover6.py
+++ rover6.py
@@ -86,8 +86,8 @@ def AtMarker4():
     px.set_motor_speed(1,(speed)*0.2)
     px.set_motor_speed(2, (-speed))
     time.sleep(0.3)
-    px.set_dir_servo_angle(-2)
-    px.set_motor_speed(1,(speed)*0.25)
+    px.set_dir_servo_angle(-4)
+    px.set_motor_speed(1,(speed)*0.22)
     px.set_motor_speed(2, (-speed))
     time.sleep(3.2)
 
